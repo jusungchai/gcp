3a8e4f5c59b96bca51eee1c56b40374b04705839
Jay <chunsaa@gmail.com>
2020-03-02 23:12:37 -0500

    add image upload component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 export default function Home() {
   const [loading, setLoading] = useState(true);
   const [msg, setMsg] = useState(null);
+  const [file, setFile] = useState("");
+  const [fileName, setFileName] = useState("Upload File");
 
   const getUsers = () => {
     return axios.get('/users');
@@ -11,13 +13,34 @@ export default function Home() {
 
   useEffect(() => {
     const users = getUsers();
-    Promise.resolve(users).then(res => {console.log(res.data); setMsg(res.data[0].name); setLoading(false)});
+    Promise.resolve(users).then(res => { console.log(res.data); setMsg(res.data[0].name); setLoading(false) });
   }, []);
-  
+
+  const handleUpload = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+    const uploaded = axios.post('/upload', formData);
+    Promise.resolve(uploaded).then(res => { console.log(res.data) }).catch(err => console.log(err));
+  }
+  const handleChange = (e) => {
+    setFile(e.target.files[0]);
+    setFileName(e.target.files[0].name);
+  }
+
   return (
     <div>
       Hi
       {loading ? null : msg}
+      <form>
+        <div>
+          <input type='file' onChange={handleChange} />
+          <label>
+            {fileName}
+          </label>
+          <button onClick={handleUpload}>Upload</button>
+        </div>
+      </form>
     </div>
   )
 }
